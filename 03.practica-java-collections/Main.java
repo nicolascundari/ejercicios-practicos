156abb5edcd4391a45ebb5073d3a8ec08b88e631
Nicolas Cundari <nicolascundari@hotmail.com>
2025-03-31 10:11:21 -0300

    Prueba usuario

--- 03.practica-java-collections/Main.java
+++ 03.practica-java-collections/Main.java
@@ -3,10 +3,7 @@ public class Main {
         Inscripcion inscripciones = new Inscripcion();
         Persona personauno = new Persona("Juan", "Perez", 20, 2939238, "11288327", "737271", "positivo");
         Circuito circuito = new CircuitoChico();
-
         inscripciones.inscribir(personauno,circuito);
-
-
         inscripciones.desinscribir(2939238);
         inscripciones.inscriptos();
     }
